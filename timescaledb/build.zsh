377f171293441fe495b8b00d9f32cb73cb8d0efa
Jakob Egger <jakob@eggerapps.at>
2025-11-19 16:27:15 +0100

    Adding TimescaleDB

---
+++ timescaledb/build.zsh
@@ -0,0 +1,69 @@
+#!/bin/zsh
+
+set -e
+
+EXTENSION_NAME=timescaledb
+EXTENSION_VERSION=2.23.0
+PG_MAJOR_VERSION=18
+PREFIX=/Applications/Postgres.app/Contents/Versions/$PG_MAJOR_VERSION
+DESTDIR=$PWD/Build
+INSTALL_ROOT=$PWD/Build/$PREFIX
+
+# make sure the correct pg_config is picked up
+export PATH=$PREFIX/bin:$PATH
+
+echo
+echo 
+echo Downloading Extension Sources
+echo
+mkdir -p Build
+curl -L https://github.com/timescale/timescaledb/archive/refs/tags/$EXTENSION_VERSION.tar.gz | tar x --cd Build
+
+# build and install the extension
+echo
+echo 
+echo "Building Extension..."
+echo
+(
+	cd Build/$EXTENSION_NAME-$EXTENSION_VERSION
+	
+	./bootstrap
+	
+	cd build
+	
+	make
+	
+	make install DESTDIR="$DESTDIR"
+)
+	
+# codesign libraries
+codesign --sign "Developer ID Application" --timestamp "$INSTALL_ROOT"/lib/postgresql/*.dylib
+
+# Build Installer Package
+echo
+echo 
+echo "Creating Installer Package..."
+echo
+mkdir -p Build/Resources
+for file in Resources/*.html distribution.xml
+do
+	sed -e "s|@EXTENSION_VERSION@|${EXTENSION_VERSION}|g" -e "s|@PG_MAJOR_VERSION@|${PG_MAJOR_VERSION}|g" -e "s|@EXTENSION_NAME@|${EXTENSION_NAME}|g" $file > Build/$file
+done
+cat Build/$EXTENSION_NAME-$EXTENSION_VERSION/tsl/LICENSE-TIMESCALE >Build/Resources/license.txt
+cat Build/$EXTENSION_NAME-$EXTENSION_VERSION/LICENSE-APACHE >>Build/Resources/license.txt
+pkgbuild --root "$INSTALL_ROOT" --install-location /Library/Application\ Support/Postgres/Extensions/$PG_MAJOR_VERSION/$EXTENSION_NAME --identifier com.postgresapp.extension.$PG_MAJOR_VERSION.$EXTENSION_NAME --sign "Developer ID Installer" --scripts Scripts $EXTENSION_NAME-$PG_MAJOR_VERSION.pkg
+productbuild --distribution Build/distribution.xml --resources Build/Resources --sign "Developer ID Installer" $EXTENSION_NAME-pg$PG_MAJOR_VERSION-$EXTENSION_VERSION.pkg
+rm $EXTENSION_NAME-$PG_MAJOR_VERSION.pkg
+
+# Notarize installer package
+echo
+echo 
+echo "Notarizing package..."
+echo
+xcrun notarytool submit $EXTENSION_NAME-pg$PG_MAJOR_VERSION-$EXTENSION_VERSION.pkg --keychain-profile postgresapp --wait
+xcrun stapler staple $EXTENSION_NAME-pg$PG_MAJOR_VERSION-$EXTENSION_VERSION.pkg
+
+echo
+echo
+echo "Done."
+echo

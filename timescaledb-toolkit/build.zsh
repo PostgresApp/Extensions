e09ccc90e59c7053710427f486e82fd806932654
Jakob Egger <jakob@eggerapps.at>
2026-04-16 14:32:17 +0200

    Fix texts

--- timescaledb-toolkit/build.zsh
+++ timescaledb-toolkit/build.zsh
@@ -2,7 +2,8 @@
 
 set -e
 
-EXTENSION_NAME=timescaledb-toolkit
+EXTENSION_NAME=timescaledb_toolkit
+REPO_NAME=timescaledb-toolkit
 EXTENSION_VERSION=1.22.0 # https://github.com/timescale/timescaledb/releases
 CARGO_PGRX_VERSION=0.16.1 # https://github.com/timescale/timescaledb-toolkit/blob/$EXTENSION_VERSION/extension/Cargo.toml
 PG_MAJOR_VERSION=18
@@ -30,7 +31,7 @@ echo
 echo Downloading Extension Sources
 echo
 mkdir -p Build
-curl -L https://github.com/timescale/timescaledb-toolkit/archive/refs/tags/$EXTENSION_VERSION.tar.gz | tar x --cd Build
+curl -L https://github.com/timescale/$REPO_NAME/archive/refs/tags/$EXTENSION_VERSION.tar.gz | tar x --cd Build
 
 # build and install the extension
 echo
@@ -38,7 +39,7 @@ echo
 echo "Building Extension..."
 echo
 (
-	cd Build/$EXTENSION_NAME-$EXTENSION_VERSION/extension
+	cd Build/$REPO_NAME-$EXTENSION_VERSION/extension
 	
 	# workaround for inttypes.h not found
 	export BINDGEN_EXTRA_CLANG_ARGS="$(xcrun --show-sdk-path | xargs -I{} echo -isysroot {})"
@@ -59,7 +60,7 @@ for file in Resources/*.html distribution.xml
 do
 	sed -e "s|@EXTENSION_VERSION@|${EXTENSION_VERSION}|g" -e "s|@PG_MAJOR_VERSION@|${PG_MAJOR_VERSION}|g" -e "s|@EXTENSION_NAME@|${EXTENSION_NAME}|g" $file > Build/$file
 done
-cat Build/$EXTENSION_NAME-$EXTENSION_VERSION/LICENSE >Build/Resources/license.txt
+cat Build/$REPO_NAME-$EXTENSION_VERSION/LICENSE >Build/Resources/license.txt
 pkgbuild --root "$INSTALL_ROOT" --install-location /Library/Application\ Support/Postgres/Extensions/$PG_MAJOR_VERSION/$EXTENSION_NAME --identifier com.postgresapp.extension.$PG_MAJOR_VERSION.$EXTENSION_NAME --sign "Developer ID Installer" --scripts Scripts $EXTENSION_NAME-$PG_MAJOR_VERSION.pkg
 productbuild --distribution Build/distribution.xml --resources Build/Resources --sign "Developer ID Installer" $EXTENSION_NAME-pg$PG_MAJOR_VERSION-$EXTENSION_VERSION.pkg
 rm $EXTENSION_NAME-$PG_MAJOR_VERSION.pkg
